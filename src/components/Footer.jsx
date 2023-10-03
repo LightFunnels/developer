3a8144374f2a14afa8d26d87b770483d88994558
Yassir Ennazk <yassir@mac-pro.home>
2023-10-03 19:59:24 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,254 @@
+import { forwardRef, Fragment, useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Transition } from '@headlessui/react'
+
+import { Button } from '@/components/Button'
+import { navigation } from '@/components/Navigation'
+
+function CheckIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
+      <circle cx="10" cy="10" r="10" strokeWidth="0" />
+      <path
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="m6.75 10.813 2.438 2.437c1.218-4.469 4.062-6.5 4.062-6.5"
+      />
+    </svg>
+  )
+}
+
+function FeedbackButton(props) {
+  return (
+    <button
+      type="submit"
+      className="px-3 text-sm font-medium text-zinc-600 transition hover:bg-zinc-900/2.5 hover:text-zinc-900 dark:text-zinc-400 dark:hover:bg-white/5 dark:hover:text-white"
+      {...props}
+    />
+  )
+}
+
+const FeedbackForm = forwardRef(function FeedbackForm({ onSubmit }, ref) {
+  return (
+    <form
+      ref={ref}
+      onSubmit={onSubmit}
+      className="absolute inset-0 flex items-center justify-center gap-6 md:justify-start"
+    >
+      <p className="text-sm text-zinc-600 dark:text-zinc-400">
+        Was this page helpful?
+      </p>
+      <div className="group grid h-8 grid-cols-[1fr,1px,1fr] overflow-hidden rounded-full border border-zinc-900/10 dark:border-white/10">
+        <FeedbackButton data-response="yes">Yes</FeedbackButton>
+        <div className="bg-zinc-900/10 dark:bg-white/10" />
+        <FeedbackButton data-response="no">No</FeedbackButton>
+      </div>
+    </form>
+  )
+})
+
+const FeedbackThanks = forwardRef(function FeedbackThanks(_props, ref) {
+  return (
+    <div
+      ref={ref}
+      className="absolute inset-0 flex justify-center md:justify-start"
+    >
+      <div className="flex items-center gap-3 rounded-full bg-blue-50/50 py-1 pr-3 pl-1.5 text-sm text-blue-900 ring-1 ring-inset ring-blue-500/20 dark:bg-blue-500/5 dark:text-blue-200 dark:ring-blue-500/30">
+        <CheckIcon className="h-5 w-5 flex-none fill-blue-500 stroke-white dark:fill-blue-200/20 dark:stroke-blue-200" />
+        Thanks for your feedback!
+      </div>
+    </div>
+  )
+})
+
+function Feedback() {
+  let [submitted, setSubmitted] = useState(false)
+
+  function onSubmit(event) {
+    event.preventDefault()
+
+    // event.nativeEvent.submitter.dataset.response
+    // => "yes" or "no"
+
+    setSubmitted(true)
+  }
+
+  return (
+    <div className="relative h-8">
+      <Transition
+        show={!submitted}
+        as={Fragment}
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        leave="pointer-events-none duration-300"
+      >
+        <FeedbackForm onSubmit={onSubmit} />
+      </Transition>
+      <Transition
+        show={submitted}
+        as={Fragment}
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        enter="delay-150 duration-300"
+      >
+        <FeedbackThanks />
+      </Transition>
+    </div>
+  )
+}
+
+function PageLink({ label, page, previous = false }) {
+  return (
+    <>
+      <Button
+        href={page.href}
+        aria-label={`${label}: ${page.title}`}
+        variant="secondary"
+        arrow={previous ? 'left' : 'right'}
+      >
+        {label}
+      </Button>
+      <Link
+        href={page.href}
+        tabIndex={-1}
+        aria-hidden="true"
+        className="text-base font-semibold text-zinc-900 transition hover:text-zinc-600 dark:text-white dark:hover:text-zinc-300"
+      >
+        {page.title}
+      </Link>
+    </>
+  )
+}
+
+function PageNavigation() {
+  let router = useRouter()
+  let allPages = navigation.flatMap((group) => group.links)
+  let currentPageIndex = allPages.findIndex(
+    (page) => page.href === router.pathname
+  )
+
+  if (currentPageIndex === -1) {
+    return null
+  }
+
+  let previousPage = allPages[currentPageIndex - 1]
+  let nextPage = allPages[currentPageIndex + 1]
+
+  if (!previousPage && !nextPage) {
+    return null
+  }
+
+  return (
+    <div className="flex">
+      {previousPage && (
+        <div className="flex flex-col items-start gap-3">
+          <PageLink label="Previous" page={previousPage} previous />
+        </div>
+      )}
+      {nextPage && (
+        <div className="ml-auto flex flex-col items-end gap-3">
+          <PageLink label="Next" page={nextPage} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+function TwitterIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+			<path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z"></path>
+		</svg>
+  )
+}
+
+function GithubIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+			<path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+		</svg>
+  )
+}
+
+function FacebookIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+		</svg>
+  )
+}
+
+function YoutubeIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-youtube" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+			<path d="M3 5m0 4a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v6a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z"></path>
+			<path d="M10 9l5 3l-5 3z"></path>
+		</svg>
+  )
+}
+
+function InstagramIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+			<path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
+			<path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+			<path d="M16.5 7.5l0 0"></path>
+		</svg>
+  )
+}
+
+function SocialLink({ href, icon: Icon, children }) {
+  return (
+    <Link target={"_blank"} href={href} className="group [transition:all_0.3s_ease] hover:[transform:translateY(-10px)]">
+      <span className="sr-only">{children}</span>
+      <Icon className="h-5 w-5 fill-zinc-700 transition group-hover:fill-zinc-900 dark:group-hover:fill-zinc-500" />
+    </Link>
+  )
+}
+
+function SmallPrint() {
+  return (
+    <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
+      <p className="text-xs text-zinc-600 dark:text-zinc-400">
+        &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+      </p>
+      <div className="flex dark:text-white gap-4">
+				<SocialLink href="https://github.com/LightFunnels" icon={GithubIcon}>
+          Follow us on Github
+        </SocialLink>
+				<SocialLink href="https://www.youtube.com/@lightfunnels" icon={YoutubeIcon}>
+          Follow us on Youtube
+        </SocialLink>
+				<SocialLink href="https://twitter.com/LightFunnels" icon={TwitterIcon}>
+          Follow us on Twitter
+        </SocialLink>
+        <SocialLink href="https://www.facebook.com/LightFunnelsLTD" icon={FacebookIcon}>
+          Follow us on Facebook
+        </SocialLink>
+				<SocialLink href="https://www.instagram.com/lightfunnelsltd/" icon={InstagramIcon}>
+          Follow us on Instagram
+        </SocialLink>
+      </div>
+    </div>
+  )
+}
+
+export function Footer() {
+  let router = useRouter()
+
+  return (
+    <footer className="mx-auto max-w-2xl space-y-10 pb-16 lg:max-w-5xl">
+      <Feedback key={router.pathname} />
+      <PageNavigation />
+      <SmallPrint />
+    </footer>
+  )
+}
